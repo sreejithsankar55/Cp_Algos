dc721f3a78e15f845763490b6755b7a558b819a9
sreejithsankar55 <32271222+sreejithsankar55@users.noreply.github.com>
2018-07-13 18:37:40 +0530

    Update bfs_stl.cpp

--- bfs_stl.cpp
+++ bfs_stl.cpp
@@ -8,50 +8,50 @@ vector < vector < ll > > g;
 
 void edge ( ll a , ll b )
 {
-	g[a].pb(b);
+    g[a].pb(b);
 }
 
 void bfs (ll u)
 {
-	queue < ll > q;
-	q.push(u);
-	v[u]=true;
-	
-	while(!q.empty())
-	{
-			ll f=q.front();
-			q.pop();
-			cout<<f<<" ";
-			
-			for (auto i = g[f].begin(); i!=g[f].end(); i++)
-			{
-				if(!v[*i])
-				{
-					q.push(*i);
-					v[*i]=true;
-				}
-			}
-	
-	}
+    queue < ll > q;
+    q.push(u);
+    v[u]=true;
+
+    while(!q.empty())
+    {
+        ll f=q.front();
+        q.pop();
+        cout<<f<<" ";
+
+        for (auto i = g[f].begin(); i!=g[f].end(); i++)
+        {
+            if(!v[*i])
+            {
+                q.push(*i);
+                v[*i]=true;
+            }
+        }
+
+    }
 
 }
 int main()
 {
-	ll a,b,n,e;// n is number of vertex, e is no. of edges
-	cin>>n>>e;
-	v.assign(n,false);
-	g.assign(n,vector<ll>());
-	
-	for( ll i = 0 ; i < n ; i++ )
-	{
-		cin>>a>>b;
-		edge(a,b);
-	}
-	for( ll i = 0 ; i < n ; i++ )
-	{
-		if(!v[i])
-			bfs(i);
-	}
+    ll a,b,n,e;// n is number of vertex, e is no. of edges
+    cin>>n>>e;
+    v.assign(n,false);
+    g.assign(n,vector<ll>());
 
-     return 0;     
- } 	   
+    for( ll i = 0 ; i < n ; i++ )
+    {
+        cin>>a>>b;
+        edge(a,b);
+    }
+    for( ll i = 0 ; i < n ; i++ )
+    {
+        if(!v[i])
+            bfs(i);
+    }
+
+    return 0;
+}
