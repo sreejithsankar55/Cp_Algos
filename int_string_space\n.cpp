13b21d269c43a218aa45c86e3f30558df498ebf9
sreejithsankar55 <32271222+sreejithsankar55@users.noreply.github.com>
2018-07-23 20:42:17 +0530

    Create int_string_space\n.cpp

---
+++ int_string_space\n.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int t;
+  cin >> t;
+  while (t--){
+    string s;
+    int n;
+    cin >> n;
+    cin.ignore();
+    getline(cin,s);
+    cout << s << endl;
+    cout << n << endl;
+    
+  }
+  return 0;
+}
