ec3d6be730c1571f0d6f29e51a597dfecddb24a6
sreejithsankar55 <32271222+sreejithsankar55@users.noreply.github.com>
2018-07-23 21:01:20 +0530

    Create stl_iinitialise.cpp

---
+++ stl_iinitialise.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+      cin>>n>>m;
+	    count = 0;
+	    vector<vector<ll> > mat(n, vector<ll> (m)), ans(n, vector<ll> (m, -1));
+	    vector<ll> flagr(n, 0), flagc(m, 0);
+      return 0;
+}
