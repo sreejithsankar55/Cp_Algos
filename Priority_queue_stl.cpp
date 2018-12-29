70e463fe6b93b39905c31f1d180bc8f3e4036102
Sreejith S <32271222+sreejithsankar55@users.noreply.github.com>
2018-12-29 23:29:31 +0530

    Create Priority_queue_stl.cpp

---
+++ Priority_queue_stl.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define vsort(v) sort(v.begin(),v.end())
+#define setpr fixed<<setprecision
+#define gl(a) getline(cin,a)
+#define ss second
+#define ll long long
+
+#define ins insert
+#define cf cout.flush()
+#define db(...) ZZ(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1> void ZZ(const char* name, Arg1&& arg1){std::cerr << name << " = " << arg1 << endl;}
+template <typename Arg1, typename... Args>void ZZ(const char* names, Arg1&& arg1, Args&&... args)
+{
+	const char* comma = strchr(names + 1, ',');
+	std::cerr.write(names, comma - names) << " = " << arg1;
+	ZZ(comma, args...);
+}
+#define countd(n) (log10(n)+1)
+#define ios ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+/*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
+//refer from Geeksforgeeks
+int main() {
+	
+	ios;
+	priority_queue <int, vector<int>, greater<int> > pq; //Asc small first
+  priority_queue <int > p; //Desc large first
+    pq.push(5); 
+    pq.push(1); 
+    pq.push(10); 
+    pq.push(30); 
+    pq.push(20); 
+  //Ascending order
+    while (pq.empty() == false) 
+    { 
+        cout << pq.top() << " "; 
+        pq.pop(); 
+    } 
+  
+    return 0;
+}
